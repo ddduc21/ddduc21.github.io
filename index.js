b3ecf73d14f13babc4518e64633486f214228a6e
ddduc21 <136151894+ddduc21@users.noreply.github.com>
2024-03-27 14:38:22 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+/** @type {HTMLInputElement} */
+const threshold = document.getElementById("threshold");
+
+/** @type {HTMLVideoElement} */
+const video = document.getElementById("video");
+
+/** @type {HTMLCanvasElement} */
+const inCanvas = document.getElementById("preprocess");
+const inContext = inCanvas.getContext("2d", { willReadFrequently: true });
+
+/** @type {HTMLCanvasElement} */
+const outCanvas = document.getElementById("processed");
+const outContext = outCanvas.getContext("2d");
+
+let width = video.videoWidth;
+let height = video.videoHeight;
+
+function draw() {
+  width = video.videoWidth;
+  height = video.videoHeight;
+
+  inCanvas.width = width;
+  inCanvas.height = height;
+  outCanvas.width = width;
+  outCanvas.height = height;
+
+  inContext.drawImage(video, 0, 0, width, height);
+  let image = inContext.getImageData(0, 0, width, height);
+  image = process(image, threshold.value);
+  outContext.putImageData(image, 0, 0);
+
+  if (video.paused || video.ended) return false;
+  setTimeout(draw, 1000 / 60);
+}
+
+video.onloadeddata = function (e) {
+  draw();
+};
+
+video.onplay = function (e) {
+  draw();
+};
+
+threshold.oninput = function (e) {
+    const value = document.getElementById("threshold-value");
+    value.innerHTML = threshold.value;
+    draw();
+}
---
+++ process.js
@@ -0,0 +1,84 @@
+/** @argument {ImageData} image*/
+function process(image, threshold) {
+  const grayscale = [];
+  const size = image.width * image.height;
+  for (let i = 0; i < size; i++) {
+    const r = image.data[i * 4];
+    const g = image.data[i * 4 + 1];
+    const b = image.data[i * 4 + 2];
+    grayscale[i] = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+  }
+
+  const gradient_x = convolution(grayscale, image.width, image.height, [
+    [1, 0, -1],
+    [2, 0, -2],
+    [1, 0, -1],
+  ]);
+
+  const gradient_y = convolution(grayscale, image.width, image.height, [
+    [ 1,  2,  1],
+    [ 0,  0,  0],
+    [-1, -2, -1],
+  ]);
+
+  for (let i = 0; i < size; i++) {
+    const g = Math.sqrt(
+      gradient_x[i] * gradient_x[i] + gradient_y[i] * gradient_y[i]
+    );
+
+		const color = sigmoid(g, threshold);
+		// const color = step(g, threshold);
+
+    image.data[i * 4] 		= color;
+    image.data[i * 4 + 1] = color;
+    image.data[i * 4 + 2] = color;
+		image.data[i * 4 + 3] = 255;
+
+    // image.data[i * 4] 		= g;
+    // image.data[i * 4 + 1] = g;
+    // image.data[i * 4 + 2] = g;
+    // image.data[i * 4 + 3] = 255;
+  }
+
+  return image;
+}
+
+/**
+ * @argument {[number]} data
+ * @argument {[[number]]} kernel
+ * */
+function convolution(data, w, h, kernel) {
+  const k_h = kernel.length;
+  const k_w = kernel[0]?.length;
+  const x_start = -(k_w / 2).toFixed(0);
+  const y_start = -(k_h / 2).toFixed(0);
+  const size = w * h;
+  const result = [];
+
+  if (!w || !h) {
+    return data;
+  }
+
+  for (let i = 0; i < size; i++) {
+    let c = 0;
+    for (let j = 0; j < k_w; j++) {
+      for (let k = 0; k < k_h; k++) {
+        const index = i + x_start + j + (y_start + k) * w;
+        if (0 <= index && index < size) {
+          c += data[index] * kernel[k][j];
+        }
+      }
+    }
+    result[i] = c;
+  }
+
+  return result;
+}
+
+function sigmoid(x, threshold) {
+	return 255 / (1 + Math.exp((x - threshold) / 5));
+}
+
+function step(x, threshold) {
+	return x >= threshold ? 0 : 255;
+}
